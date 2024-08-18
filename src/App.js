1e323c4dfa11a93a62f3b90f1f8dfd28a59b9cd0
Arnold Kiritu <arnoldkells4@gmail.com>
2024-08-18 18:42:40 +0300

    have recreated this project and hosted it for the first time

--- src/App.js
+++ src/App.js
@@ -3,8 +3,6 @@ import AboutUs from "./pages/AboutPage";
 import Contact from "./pages/ContacPage";
 import ScrollToTop from "./components/assets/ScrollToTop";
 import Footer from "./components/Footer";
-import { ShopContext } from "./components/assets/ShopContext";
-import Cart from "./pages/cart/ShoppingCart";
 import Home from "./pages/home/HomePage";
 import Service from "./pages/services/ServicesPage";
 
@@ -19,7 +17,6 @@ function App() {
           <Route path="/service" element={<Service/>}/>
           <Route path="/about" element={<AboutUs/>}/>
           <Route path="/contact" element={<Contact/>}/>
-          <Route path="/cart" element={<Cart/>}/>
         </Routes>
         <Footer/>
       </Router>
